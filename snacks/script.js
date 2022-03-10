f35617778009e0aa81937e1629fee77ebade4d4d
camillapalombi <camilla.palombi14@gmail.com>
2022-03-10 19:14:02 +0100

    JS Snack 2.

--- snacks/script.js
+++ snacks/script.js
@@ -1,6 +1,6 @@
 // A partire da un array di stringhe, crea un secondo array formattando
 // le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
-
+/*
 const arrNames = ['Camilla', 'FRANCO', 'giulio', 'CheCco']
 
 let correctArrNames = arrNames.map((names) => {
@@ -11,5 +11,45 @@ let correctArrNames = arrNames.map((names) => {
     let resto = minuscole.substring(1);
     return maiuscole + resto;
 });
-console.log(correctArrNames)
+console.log(correctArrNames)*/
 
+
+
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+
+const arrObjAnimals = [
+    {
+        name : 'Leone',
+        family : 'Felidi',
+        class : 'Mammiferi'
+    },
+    {
+        name : 'Cane',
+        family : 'Canidi',
+        class : 'Mammiferi'
+    },
+    {
+        name : 'Gallina',
+        family : 'fasianidi',
+        class : 'Uccelli'
+    },
+    {
+        name : 'Aquila',
+        family : 'Accipitridae',
+        class : 'Uccelli'
+    },
+    {
+        name : 'Trota',
+        family : 'Salmonidae',
+        class : 'Pesci'
+    }
+];
+
+const arrMammiferi = arrObjAnimals.filter((elemento) => {
+    if (elemento.class == 'Mammiferi') {
+        return true;
+    }
+    return false;
+});
+console.log(arrMammiferi);
