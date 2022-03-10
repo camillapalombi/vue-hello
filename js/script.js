2f330ca5afdc8059cb942e15291ecf16c83357d6
camillapalombi <camilla.palombi14@gmail.com>
2022-03-10 15:29:50 +0100

    JS Stampa messaggio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//1. Stampa a schermo un messaggio tramite i data di VUE all'interno di un h1:
+
+const app = new Vue({
+
+    el : "#app",
+    data : {
+        message : 'Hello World!'
+    }
+});
